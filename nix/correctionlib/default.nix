7fe0e1ff8da8a7628a9f5b9a8b72b4c5c366d0fa
Yipeng Sun <syp@umd.edu>
2023-05-16 22:51:29 +0800

    Add a working correctionlib pkg.

---
+++ nix/correctionlib/default.nix
@@ -0,0 +1,27 @@
+
+{ lib
+, buildPythonPackage
+, fetchPypi
+, setuptools-scm
+, scikit-build
+, cmake
+, numpy
+, pydantic
+, rich
+}:
+
+buildPythonPackage rec {
+  pname = "correctionlib";
+  version = "2.2.2";
+
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "sha256-h3eggtPLSF/8ShQ5xzowZW1KSlcI/YBsPu3lsSyzHkw=";
+  };
+
+  nativeBuildInputs = [ cmake setuptools-scm scikit-build ];
+  propagatedBuildInputs = [ numpy pydantic rich ];
+
+  dontUseCmakeConfigure = true;
+  doCheck = false;
+}
--- nix/overlay.nix
+++ nix/overlay.nix
@@ -2,7 +2,8 @@ final: prev:
 
 {
   pythonOverrides = prev.lib.composeExtensions prev.pythonOverrides (finalPy: prevPy: {
-    coffea = finalPy.callPackage ./coffea/default.nix { };
+    correctionlib = finalPy.callPackage ./correctionlib { };
+    coffea = finalPy.callPackage ./coffea { };
   });
   python3 = prev.python3.override { packageOverrides = final.pythonOverrides; };
 }
